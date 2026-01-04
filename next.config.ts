dcd2f711c0154e5f8f475cb0870ba18841cdf224
Your Name <you@example.com>
2026-01-04 20:19:06 +0100

    google drive

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true, // <-- move it here
+  reactCompiler: true,
   images: {
     remotePatterns: [
       {
@@ -11,11 +11,21 @@ const nextConfig: NextConfig = {
       { 
         protocol: "https", 
         hostname: "firebasestorage.googleapis.com", 
-        pathname: "/v0/b/**" 
+        pathname: "/v0/b/**",
       },
       {
         protocol: "https",
         hostname: "cdn.pixabay.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "drive.google.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
         pathname: "/**",
       },
     ],
